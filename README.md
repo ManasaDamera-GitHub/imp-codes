4fef00f2960a62d8a1e3ca7bf46520f9b542c922
DameraManasa-GitHub <dameramanasa25@gmail.com>
2025-03-10 19:32:07 +0530

    changed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # imp-codes
+
+fs.js---> crud operations on new.json file.checking if the same name presents or not in the file.
---
+++ fs.js
@@ -0,0 +1,35 @@
+let express = require("express");
+const fs = require("fs");
+let app = express();
+// console.log(app);
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+  let data = fs.readFile("./new.json", "utf8");
+  res.send(data);
+});
+
+app.post("/names", async (req, res) => {
+  try {
+    let inputData = req.body;
+    let existData = JSON.parse(await fs.readFile("./new.json", "utf8"));
+    let isExistAlready = existing.find((x, y) => {
+      return x.name == inputData.name;
+    });
+    console.log(isExistAlready, "exist");
+    if (isExistAlready) {
+      res.status(400).send({ message: "username already exists" });
+    } else {
+      existData.push(inputData);
+      let updatedData = existData;
+      await fs.writeFile("./new.json", JSON.stringify(updatedData));
+      res.send("data inserted");
+    }
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("server created");
+});
